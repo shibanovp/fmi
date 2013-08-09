de6e56acbf2d4c3075ff7d1b4a3db8236c947095
paul.shibanov@gmail.com <>
2013-08-09 12:14:15 +0000

    Added FmiStudent module

---
+++ module/FmiStudent/config/module.config.php
@@ -0,0 +1,31 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'FmiStudent\Controller\Index' => 'FmiStudent\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'album' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/fmi-student[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'FmiStudent\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
+);
