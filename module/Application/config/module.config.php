0f6aeb5889d7ecc47764f605218aad5a6a729cfa
paul.shibanov@gmail.com <>
2013-08-09 11:17:42 +0000

    Added FmiStudentController

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -73,7 +73,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\FmiStudent' => 'Application\Controller\FmiStudentController'
         ),
     ),
     'view_manager' => array(
