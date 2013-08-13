3ea1e05d1ef5064cb2b9376debc1c29817e91ce8
paul.shibanov@gmail.com <>
2013-08-13 10:00:33 +0000

    add dojo

--- module/FmiStudent/src/FmiStudent/Controller/IndexController.php
+++ module/FmiStudent/src/FmiStudent/Controller/IndexController.php
@@ -35,6 +35,12 @@ class IndexController extends AbstractActionController
         $viewModel = new ViewModel();
         $this->layout('layout/FmiStudent');//add into module.config
         return $viewModel;
+    }
+    public function dojoAjaxAction()
+    {
+        $viewModel = new ViewModel();
+        $this->layout('layout/FmiStudent');//add into module.config
+        return $viewModel;
     }    
     public function serviceAjaxAction()
     {
